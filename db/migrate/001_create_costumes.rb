16a20f29631d573ad0ae468862a7c1ff10185cca
github username <github email address>
2019-07-27 15:02:31 -0400

    Done.

--- db/migrate/001_create_costumes.rb
+++ db/migrate/001_create_costumes.rb
@@ -1,11 +1,12 @@
 # Create a class and inherit from ActiveRecord::Migration
-class CreateCostumes < ActiveRecord::Migration
+class CreateCostumes < ActiveRecord::Migration[4.2]
     def change
         create_table :costumes do |t|
             t.string :name
             t.integer :price
             t.string :size
             t.string :image_url
+            t.timestamps
         end 
     end 
 end 
